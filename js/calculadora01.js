dceb3fa26bbbbf2a2ae625dd5f5957ba11fcba74
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-05-02 17:24:00 -0300

    Configurando o template

--- js/calculadora01.js
+++ js/calculadora01.js
@@ -1,15 +1,20 @@
 var fullSoma = 0;
+
 function soma(){
-    var n1 = document.getElementById("campo1").value;
-    var n2 = document.getElementById("campo2").value;
-    var soma = parseInt(n1) + parseInt(n2);
-    fullSoma = fullSoma + soma;
-    lb01.innerHTML = "Soma é igual: " + soma;
 
-    var linha = document.createElement("tr");
-    linha.innerHTML = "<td>" + n1 +"</td><td>" + n2 + "</td><td>" + soma + "</td>";
-    document.getElementById("table-tbody").appendChild(linha);
+    var n1 = parseInt(document.getElementById("campo1").value);
+    var n2 = parseInt(document.getElementById("campo2").value);
 
-    document.getElementById("table-tfoot").innerHTML = "Total soma: " + fullSoma;
+    if(!isNaN(n1) && !isNaN(n2)){
+        var soma = n1 + n2;
+        fullSoma = fullSoma + soma;
+        lb01.innerHTML = "Soma é igual: " + soma;
+    
+        var linha = document.createElement("tr");
+        linha.innerHTML = "<td>" + n1 +"</td><td>" + n2 + "</td><td>" + soma + "</td>";
+        document.getElementById("table-tbody").appendChild(linha);
+    
+        document.getElementById("table-tfoot").innerHTML = "Total soma: " + fullSoma;
+    }
 }
 
